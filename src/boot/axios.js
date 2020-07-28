5f93b3592a6b27323f8f3a60dd505e2f6c12e960
Meursault57 <verdam@data.pl>
2020-07-28 17:13:32 +0200

    ...

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,12 +2,3 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$axios = axios
-
-const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8081'
-})
-
-export default ({ Vue }) => {
-  Vue.prototype.$axios = axiosInstance
-}
-export { axiosInstance }
