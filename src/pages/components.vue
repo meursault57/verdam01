886da17ced5735f72a4bfaa50d134345d67eb3a4
Meursault57 <verdam@data.pl>
2020-07-29 17:29:17 +0200

    ...

--- src/pages/components.vue
+++ src/pages/components.vue
@@ -1,6 +1,21 @@
 <template>
 <div>
-  <q-btn @click="doSomething" label="Do something" />
-  <!-- <q-icon name="alarm" /> -->
+  <q-btn @click="doSomething1" label="Click!" />
+  <q-icon name="alarm" />
 </div>
 </template>>
+
+<script>
+
+export default {
+  methods: {
+    doSomething1 () {
+      alert('doopa')
+    },
+    doSomething2: function () {
+      alert('doopa')
+    }
+  }
+}
+
+</script>>
