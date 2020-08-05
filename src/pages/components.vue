8e618ae16e3fda7e479301d0f73e34a6fe959a61
Meursault57 <verdam@data.pl>
2020-08-05 11:31:15 +0200

    ...

--- src/pages/components.vue
+++ src/pages/components.vue
@@ -2,6 +2,8 @@
 <template>
   <div>
     <q-input v-model='doopa'></q-input>
+    <q-btn @click="doopa=''">Clear.L</q-btn>
+    <q-btn @click="clearMessage">Clear.M</q-btn>
     <h5>{{ doopa }}</h5>
   </div>
 </template>
@@ -12,6 +14,17 @@ export default {
     return {
       doopa: 'Wpis do komponentu, konstruktor.'
     }
+  },
+  methods: {
+    clearMessage () {
+      this.doopa = ''
+    }
   }
 }
 </script>
+
+<style>
+h5 {
+color: purple
+}
+</style>
