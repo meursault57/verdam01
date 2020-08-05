ca2a9474b3fc75ef2e05825ba6be37b1b891608c
Meursault57 <verdam@data.pl>
2020-08-05 13:14:43 +0200

    ...

--- src/pages/components.vue
+++ src/pages/components.vue
@@ -1,10 +1,12 @@
 
 <template>
   <div>
-    <q-input v-model='doopa'></q-input>
+    <q-page padding>
+    <q-input v-model="doopa" @keyup.space="handleKeyup" />
     <q-btn @click="doopa=''">Clear.L</q-btn>
     <q-btn @click="clearMessage">Clear.M</q-btn>
     <h5>{{ doopa }}</h5>
+    </q-page>
   </div>
 </template>
 
@@ -18,6 +20,12 @@ export default {
   methods: {
     clearMessage () {
       this.doopa = ''
+    },
+    handleKeyup (e) {
+      console.log(e)
+      // if (e.keyCode === 32) {
+      this.clearMessage()
+      // }
     }
   }
 }
