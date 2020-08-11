ec472248b467e2c80cd96f18363824e22508e7ad
Meursault57 <verdam@data.pl>
2020-08-11 09:38:18 +0200

    ...

--- src/pages/citrone.vue
+++ src/pages/citrone.vue
@@ -1,7 +1,10 @@
 <template>
     <q-page padding>
       <ul>
-        <li v-for="task in tasks" :key="task">{{ task }}</li>
+        <li v-for="(task, key) in tasks" :key="key">
+          <div>{{ task.name }} ({{ key }})</div>
+          <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+          </li>
       </ul>
     </q-page>
 </template>
@@ -11,7 +14,29 @@ var APP_LOG_LIFECYCLE_EVENTS = true
 export default {
   data () {
     return {
-      tasks: ['Go to shop', 'Get bananas', 'Get apples']
+    /* tasks: ['Go to shop', 'Get bananas', 'Get apples', 'Get Kotelet'] */
+      tasks: [
+        {
+          name: 'Go to shop',
+          dueDate: '2019/05/12',
+          dueTime: '18:30'
+        },
+        {
+          name: 'Get bananas',
+          dueDate: '2019/05/13',
+          dueTime: '14:00'
+        },
+        {
+          name: 'Get apples',
+          dueDate: '2019/05/14',
+          dueTime: '16:00'
+        },
+        {
+          name: 'Get Kotelet',
+          dueDate: '2019/05/15',
+          dueTime: '18:00'
+        }
+      ]
     }
   },
 
