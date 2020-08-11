54e2e061174198075e613aa186466087e7190189
Meursault57 <verdam@data.pl>
2020-08-11 08:28:13 +0200

    ...

---
+++ src/pages/citrone.vue
@@ -0,0 +1,61 @@
+<template>
+    <q-page padding>
+
+    </q-page>
+</template>
+
+<script>
+var APP_LOG_LIFECYCLE_EVENTS = true
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  // LIFECYCLE_EVENTS ////////////////
+  beforeCreate: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****beforeCreate')
+    }
+  },
+  created: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****created')
+    }
+  },
+  beforeMount: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****beforeMount')
+    }
+  },
+  mounted: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****mounted')
+    }
+  },
+  beforeUpdate: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****beforeUpdate')
+    }
+  },
+  updated: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****updated')
+    }
+  },
+  beforeDestroy: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****beforeDestroy')
+    }
+  },
+  destroyed: function () {
+    if (APP_LOG_LIFECYCLE_EVENTS) {
+      console.log('*****destroyed')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
