c8702fd4b6888bcddad35de0127c663ab48b2fab
Meursault57 <verdam@data.pl>
2020-08-11 11:59:24 +0200

    components ~~

---
+++ src/components/Task.vue
@@ -0,0 +1,13 @@
+<template>
+        <li v-for="(task, key) in tasks" :key="key">
+          <div>{{ task.name }} ({{ key }})</div>
+          <small>{{ task.dueDate }} @ {{ task.dueTime }}</small>
+          <button @click="deleteTask(key)">xy</button>
+          </li>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
